5c11289050328fbc0edacd4f7775fe12aff3d8bc
Nodematic Labs <community@nodematic.com>
2025-10-15 03:06:33 +0000

    Initial commit

---
+++ s3creds.sh
@@ -0,0 +1,5 @@
+export AWS_HOST=$(kubectl -n rook-ceph get cm demo-bucket -o jsonpath='{.data.BUCKET_HOST}')
+export PORT=$(kubectl -n rook-ceph get cm demo-bucket -o jsonpath='{.data.BUCKET_PORT}')
+export BUCKET_NAME=$(kubectl -n rook-ceph get cm demo-bucket -o jsonpath='{.data.BUCKET_NAME}')
+export AWS_ACCESS_KEY_ID=$(kubectl -n rook-ceph get secret demo-bucket -o jsonpath='{.data.AWS_ACCESS_KEY_ID}' | base64 --decode)
+export AWS_SECRET_ACCESS_KEY=$(kubectl -n rook-ceph get secret demo-bucket -o jsonpath='{.data.AWS_SECRET_ACCESS_KEY}' | base64 --decode)
